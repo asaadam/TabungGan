20f9c1623ac3d96427a0723e48ce6d30bcb52a27
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2019-11-27 22:33:00 +0700

    Add helpers month and year picker

--- app/src/main/java/com/example/tabungangan/fragments/Transaksi.java
+++ app/src/main/java/com/example/tabungangan/fragments/Transaksi.java
@@ -1,21 +1,56 @@
 package com.example.tabungangan.fragments;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.tabungangan.R;
+import com.example.tabungangan.helpers.MonthYearPicker;
 
-public class Transaksi extends Fragment {
+public class Transaksi extends Fragment implements View.OnClickListener{
+
+    TextView coba;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_transaksi, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        coba = view.findViewById(R.id.coba);
+//
+//        coba.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+//            case R.id.coba:
+//                final Context context = v.getContext();
+//                MonthYearPicker pickerDialog = new MonthYearPicker();
+//                pickerDialog.show(getFragmentManager(), "MonthYearPicker");
+//                pickerDialog.setListener(new DatePickerDialog.OnDateSetListener() {
+//                    @Override
+//                    public void onDateSet(DatePicker datePicker, int year, int month, int i2) {
+//                        Toast.makeText(context, month + "-" + year, Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/tabungangan/helpers/MonthYearPicker.java
@@ -0,0 +1,65 @@
+package com.example.tabungangan.helpers;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.NumberPicker;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.tabungangan.R;
+
+import java.util.Calendar;
+
+public class MonthYearPicker extends DialogFragment {
+    private static final int MAX_YEAR = 2099;
+    private DatePickerDialog.OnDateSetListener listener;
+
+    public void setListener(DatePickerDialog.OnDateSetListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        Calendar cal = Calendar.getInstance();
+
+        View dialog = inflater.inflate(R.layout.helper_month_year_picker, null);
+        final NumberPicker monthPicker = (NumberPicker) dialog.findViewById(R.id.picker_month);
+        final NumberPicker yearPicker = (NumberPicker) dialog.findViewById(R.id.picker_year);
+
+        monthPicker.setMinValue(1);
+        monthPicker.setMaxValue(12);
+        monthPicker.setValue(cal.get(Calendar.MONTH) + 1);
+
+        int year = cal.get(Calendar.YEAR);
+        yearPicker.setMinValue(2000);
+        yearPicker.setMaxValue(year);
+        yearPicker.setValue(year);
+
+        builder.setView(dialog)
+                // Add action buttons
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        listener.onDateSet(null, yearPicker.getValue(), monthPicker.getValue(), 0);
+                    }
+                })
+                .setNegativeButton(R.string.batal, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MonthYearPicker.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+}
